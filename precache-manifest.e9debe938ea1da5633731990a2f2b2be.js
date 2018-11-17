e7cd1f2e135544b1a06602b5ad020e9753120d94
MADHAN <madhansmart7404@gmail.com>
2018-11-17 18:34:23 +0530

    Updates

---
+++ precache-manifest.e9debe938ea1da5633731990a2f2b2be.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0b236c4cf688f4dcb80307c6832d21d",
+    "url": "/Dinnergise/static/media/main-content.d0b236c4.jpg"
+  },
+  {
+    "revision": "07d3c92635201c127d0f",
+    "url": "/Dinnergise/static/css/main.2b4ded40.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Dinnergise/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "bea110c6ddb60ed9e5b7",
+    "url": "/Dinnergise/static/js/1.bea110c6.chunk.js"
+  },
+  {
+    "revision": "469b16ad7af89ab95c06",
+    "url": "/Dinnergise/static/js/runtime~main.469b16ad.js"
+  },
+  {
+    "revision": "05158509544a82d79ef5536b3ad45a7a",
+    "url": "/Dinnergise/static/media/logo.05158509.png"
+  },
+  {
+    "revision": "5d978871438c92559da3af1ed0e17509",
+    "url": "/Dinnergise/static/media/background1.5d978871.jpg"
+  },
+  {
+    "revision": "07d3c92635201c127d0f",
+    "url": "/Dinnergise/static/js/main.07d3c926.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Dinnergise/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Dinnergise/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Dinnergise/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Dinnergise/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "bea110c6ddb60ed9e5b7",
+    "url": "/Dinnergise/static/css/1.51ec9383.chunk.css"
+  },
+  {
+    "revision": "aee4aa192e867827def76a6a327fd9eb",
+    "url": "/Dinnergise/index.html"
+  }
+];
